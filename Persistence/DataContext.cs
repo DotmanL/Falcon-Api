ce8e7c76c0d35b8c86f5a376057c11c11049f7fd
oladotun lawal <oladotun@bindystreet.com>
2022-06-27 00:07:46 +0100

    created db context and activities properties

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        // sets the name of our table to Activities with the type of Activity
+        //contains all the columns in our table
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
